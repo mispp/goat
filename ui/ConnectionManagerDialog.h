9b5660c9aa24c30a3aac710b1a91f576bf7bc027
mispp <mspehar@gmail.com>
2018-08-02 19:01:04 +0200

    add back connection manager dialog and change connection tab to include buttons

---
+++ ui/ConnectionManagerDialog.h
@@ -0,0 +1,48 @@
+#ifndef CONNECTIONMANAGERDIALOG_H
+#define CONNECTIONMANAGERDIALOG_H
+
+#include <QDialog>
+#include <QStandardItemModel>
+#include <QAbstractButton>
+#include <QListWidgetItem>
+#include <QStandardItemModel>
+#include <QStandardItem>
+#include <QDataWidgetMapper>
+
+#include "src/ConnectionStandardItem.h"
+
+namespace Ui {
+class ConnectionManagerDialog;
+}
+
+class ConnectionManagerDialog : public QDialog
+{
+	Q_OBJECT
+
+public:
+    explicit ConnectionManagerDialog(QWidget *parent = 0);
+    ~ConnectionManagerDialog();
+
+private slots:
+	void on_buttonBox_accepted();
+	void on_buttonBox_rejected();
+	void on_buttonBox_clicked(QAbstractButton *button);
+    void on_buttonNewConnection_released();
+    void on_listViewSelectionChanged(QItemSelection current, QItemSelection previous);
+    void on_rowsInserted(const QModelIndex &source_parent, int start, int end);
+    void on_renameListViewItem(QStandardItem* changedItem);
+
+private:
+    Ui::ConnectionManagerDialog *ui;
+
+    QStandardItemModel* m_model;
+    QStandardItemModel* m_driversModel;
+    QStandardItemModel* m_connectionListModel;
+    QDataWidgetMapper* m_dataWidgetMapper;
+    void updateConnectionListModel();
+    void updateCurrentlySelectedConnection();
+    QMap<QString, QVariant> getSelectedConnectionDefinition();
+    ConnectionStandardItem* getCurrentlySelectedConnection();
+};
+
+#endif // CONNECTIONMANAGERDIALOG_H
